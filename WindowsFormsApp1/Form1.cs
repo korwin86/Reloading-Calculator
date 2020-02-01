2cdec1d65c81033422d93daefec3672a7e70df78
Александр Семенов <korwin86@gmail.com>
2020-02-01 16:03:18 +0700

    Revert "Revert "Revert "test"""
    This reverts commit a7b21eaa965c4ed3329eae0ef337580b408884b4.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,7 +10,7 @@ namespace WindowsFormsApp1
             InitializeComponent();
 
         }
-        //test
+
         private void Button1_Click(object sender, EventArgs e)
         {
             string result;
