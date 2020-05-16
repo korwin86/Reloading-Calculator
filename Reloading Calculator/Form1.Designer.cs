824ffc975d074cbd8e3059b0df4f24911d89e714
Александр Семенов <korwin86@gmail.com>
2020-05-17 01:54:58 +0700

    упрощенная форма

--- Reloading Calculator/Form1.Designer.cs
+++ Reloading Calculator/Form1.Designer.cs
@@ -754,7 +754,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Reloading Calculator";
+            this.Text = "SAC A";
             this.ResumeLayout(false);
             this.PerformLayout();
 
